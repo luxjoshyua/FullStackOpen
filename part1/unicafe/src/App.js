962bd6f61d07e097e653a6b702b3bd50f15c7681
Joshua Fielding <josh.e.fielding@gmail.com>
2023-01-26 10:06:14 +1100

    added conditional rendering

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,43 +5,74 @@ import './App.css';
 const Header = ({ title }) => <h1>{title}</h1>;
 
 // Button
-const Button = ({ name, onClick }) => <button onClick={onClick}>{name}</button>;
+const Button = ({ name, onClick }) => {
+  // listten if the  button has been clicked
+  const hasValue = true;
+  return <button onClick={onClick}>{name}</button>;
+};
 
 // Statistics
-const Statistics = ({ name, value }) => (
-  <p>
-    {name}: {value}
-  </p>
-);
+const Statistics = ({ name, value }) => {
+  // console.log(value);
+
+  return (
+    <p>
+      {name}: {value}
+    </p>
+  );
+};
 
 const App = () => {
   // save clicks of each button to their own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [isClicked, setIsClicked] = useState(false);
+
+  function handleClick() {
+    setIsClicked(true);
+  }
 
   return (
     <div>
       <Header title="Give Feedback" />
-      {/* Buttons in a wrapper */}
-      <Button name="good" onClick={() => setGood(good + 1)} />
-      <Button name="neutral" onClick={() => setNeutral(neutral + 1)} />
-      <Button name="bad" onClick={() => setBad(bad + 1)} />
-      <Header title="Statistics" />
-      <Statistics name="good" value={good} />
-      <Statistics name="neutral" value={neutral} />
-      <Statistics name="bad" value={bad} />
-      <Statistics name="all" value={good + neutral + bad} />
-
-      {/* need these in percentages */}
-      <Statistics name="average" value={((good - bad) / (good + neutral + bad)).toFixed(2)} />
-
-      {/* need these in percentages */}
-
-      <Statistics
-        name="positive"
-        value={((good / (good + neutral + bad)) * 100).toFixed(2) + `%`}
+      <Button
+        name="good"
+        onClick={() => {
+          setGood(good + 1);
+          handleClick();
+        }}
       />
+      <Button
+        name="neutral"
+        onClick={() => {
+          setNeutral(neutral + 1);
+          handleClick();
+        }}
+      />
+      <Button
+        name="bad"
+        onClick={() => {
+          setBad(bad + 1);
+          handleClick();
+        }}
+      />
+      <Header title="Statistics" />
+      {isClicked === false ? (
+        <p>No feedback given</p>
+      ) : (
+        <>
+          <Statistics name="good" value={good} />
+          <Statistics name="neutral" value={neutral} />
+          <Statistics name="bad" value={bad} />
+          <Statistics name="all" value={good + neutral + bad} />
+          <Statistics name="average" value={((good - bad) / (good + neutral + bad)).toFixed(2)} />
+          <Statistics
+            name="positive"
+            value={((good / (good + neutral + bad)) * 100).toFixed(2) + `%`}
+          />
+        </>
+      )}
     </div>
   );
 };
