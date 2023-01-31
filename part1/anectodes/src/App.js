c2fd566b60bafe2eedfd3372a6021194ae309a09
Joshua Fielding <josh.e.fielding@gmail.com>
2023-01-31 17:52:57 +1100

    added final steps

--- part1/anectodes/src/App.js
+++ part1/anectodes/src/App.js
@@ -4,31 +4,24 @@ const Button = ({ text, onClick }) => {
   return <button onClick={onClick}>{text}</button>;
 };
 
+const Heading = ({ text }) => <h2>{text}</h2>;
+
 const App = () => {
   const anecdotes = [
-    // 0
     'If it hurts, do it more often.',
-    // 1
     'Adding manpower to a late software project makes it later!',
-    // 2
     'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    // 3
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    // 4
     'Premature optimization is the root of all evil.',
-    // 5
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    // 6
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    // 7
     'The only way to go fast, is to go well.',
   ];
-
-  // const randomNum = Math.floor(Math.random() * anecdotes.length);
 
   // so the initial phrase is always random
   const [selected, setSelected] = useState(0);
   const [isClicked, setIsClicked] = useState(false);
+  const [highestNumber, setHighestNumber] = useState(0);
 
   const handleInitialClick = () => {
     setIsClicked(true);
@@ -42,11 +35,18 @@ const App = () => {
   const points = new Array(8).fill(0);
   const [votes, setVotes] = useState(points);
   const handleVoteChange = () => {
-    // never mutate state directly
+    // never mutate state directly so make a copy
     const copyOfVotesArray = [...votes];
+    // increase for the selected anectode
     copyOfVotesArray[selected] += 1;
     setVotes(copyOfVotesArray);
+
+    // set the highest voted number
+    const highest = Math.max(...copyOfVotesArray);
+    setHighestNumber(highest);
   };
+
+  const mostVoted = anecdotes[votes.indexOf(Math.max(...votes))];
 
   return (
     <div>
@@ -57,11 +57,18 @@ const App = () => {
         </>
       ) : (
         <div>
-          {/* do two separate components for different sections */}
-          <p>{anecdotes[selected]}</p>
-          <p>Anectode has {votes[selected]} number of votes</p>
-          <Button text="vote" onClick={handleVoteChange} />
-          <Button text="Next anectode" onClick={handleMoreClick} />
+          <>
+            <Heading text="Anectode of the day" />
+            <p>{anecdotes[selected]}</p>
+            <p>Anectode has {votes[selected]} number of votes</p>
+            <Button text="vote" onClick={handleVoteChange} />
+            <Button text="Next anectode" onClick={handleMoreClick} />
+          </>
+          <>
+            <Heading text="Anectode with most votes" />
+            <p>{mostVoted}</p>
+            <p>Has {highestNumber} votes</p>
+          </>
         </div>
       )}
     </div>
