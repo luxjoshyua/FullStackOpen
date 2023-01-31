af79437ebbe5c7cf2bb379257e76df9583763aa2
Joshua Fielding <josh.e.fielding@gmail.com>
2023-02-01 05:25:45 +1100

    removed commented code

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -95,49 +95,6 @@ const App = () => {
       )}
     </div>
   );
-
-  // return (
-  //   <div>
-  //     <Header title="Give Feedback" />
-  //     <Button
-  //       name="good"
-  //       onClick={() => {
-  //         setGood(good + 1);
-  //         handleClick();
-  //       }}
-  //     />
-  //     <Button
-  //       name="neutral"
-  //       onClick={() => {
-  //         setNeutral(neutral + 1);
-  //         handleClick();
-  //       }}
-  //     />
-  //     <Button
-  //       name="bad"
-  //       onClick={() => {
-  //         setBad(bad + 1);
-  //         handleClick();
-  //       }}
-  //     />
-  //     <Header title="Statistics" />
-  //     {isClicked === false ? (
-  //       <p>No feedback given</p>
-  //     ) : (
-  //       <>
-  //         <Statistics name="good" value={good} />
-  //         <Statistics name="neutral" value={neutral} />
-  //         <Statistics name="bad" value={bad} />
-  //         <Statistics name="all" value={good + neutral + bad} />
-  //         <Statistics name="average" value={((good - bad) / (good + neutral + bad)).toFixed(2)} />
-  //         <Statistics
-  //           name="positive"
-  //           value={((good / (good + neutral + bad)) * 100).toFixed(2) + `%`}
-  //         />
-  //       </>
-  //     )}
-  //   </div>
-  // );
 };
 
 export default App;
