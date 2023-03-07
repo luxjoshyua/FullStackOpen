b8e12190ad91d3687c863a3e3b07d59f32d28458
Joshua Fielding <josh.e.fielding@gmail.com>
2023-03-08 09:26:21 +1100

    part3 - test 2

--- part3/course/frontend/src/App.js
+++ part3/course/frontend/src/App.js
@@ -40,37 +40,6 @@ const App = () => {
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
-  // conditional rendering, resolve problem of notes being null on initial load
-  // if (!notes) {
-  //   return null;
-  // }
-
-  // const toggleImportanceOf = (id) => {
-  //   console.log(`importance of ${id} needs to be toggled`);
-  //   // unique url for each note resource based on its id
-  //   // const url = `http://localhost:3001/notes/${id}`;
-  //   const note = notes.find((n) => n.id === id);
-  //   // spread into new object, flip the value of important property (from true to false, false to true), as it overrides in the spread
-  //   const changedNote = { ...note, important: !note.important };
-
-  //   noteService
-  //     .update(id, changedNote)
-  //     .then((returnedNote) => {
-  //       setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
-  //     })
-  //     .catch((error) => {
-  //       alert(`the note ${note.content} was already deleted from server, throw ${error}`);
-  //       setErrorMessage(`Note ${note.content} was already removed from server`);
-  //       // reset the error message after 5 seconds
-  //       setTimeout(() => {
-  //         setErrorMessage(null);
-  //       }, 5000);
-  //       // deleted note gets filtered out from the state
-  //       // filter returns new array
-  //       setNotes(notes.filter((n) => n.id !== id));
-  //     });
-  // };
-
   const toggleImportanceOf = (id) => {
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
@@ -103,12 +72,7 @@ const App = () => {
       <ul>
         {notesToShow.map((note) => (
           // each note receives its own event handler because the id of every note is unique
-          <Note
-            key={note.id}
-            note={note}
-            toggleImportance={() => toggleImportanceOf(note.id)}
-            className="note"
-          />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
         ))}
       </ul>
       <form onSubmit={addNote}>
