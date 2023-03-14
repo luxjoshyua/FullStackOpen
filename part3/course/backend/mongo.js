5b80e64501a0763993fcd8d359a585df63832946
Joshua Fielding <josh.e.fielding@gmail.com>
2023-03-15 06:53:08 +1100

    part3 - mongo connected with async method

--- part3/course/backend/mongo.js
+++ part3/course/backend/mongo.js
@@ -8,24 +8,43 @@ if (process.argv.length < 3) {
 const password = process.argv[2];
 const url = `mongodb+srv://josh:${password}@cluster0.can6okv.mongodb.net/?retryWrites=true&w=majority`;
 
-// rework this into a try catch
-mongoose
-  .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('MongoDB Connected');
-    note.save();
-    console.log('New note added!');
-  })
-  .catch((error) => {
-    console.log(`Oh no, error: ${error}`);
-    // mongoose.connection.close();
-  });
-// .then(() => {
-//   mongoose.disconnect();
-// });
+// mongoose - works but is much worse than async method
+//   .connect(url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log('MongoDB Connected');
+//     note.save();
+//     console.log('New note added!');
+//   })
+//   .catch((error) => {
+//     console.log(`Oh no, error: ${error}`);
+//   });
+
+const dbConnect = async () => {
+  let db = null;
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = mongoose.connection;
+    console.log('connected!');
+
+    let dbRes = await note.save();
+    db.close();
+    return dbRes;
+  } catch (error) {
+    db && db.close();
+    console.log(`Error in database connection: ${error}`);
+    throw error;
+  }
+};
+
+dbConnect()
+  .then((res) => console.log(`Connected to db: ${res}`))
+  .catch((error) => console.log(`Error calling: ${error}`));
 
 const noteSchema = new mongoose.Schema({
   content: String,
@@ -36,7 +55,7 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model('Note', noteSchema);
 
 const note = new Note({
-  content: 'Adding even more test content is fun fun fun!',
+  content: 'Adding heaps heaps more test content!',
   date: new Date(),
   important: true,
 });
