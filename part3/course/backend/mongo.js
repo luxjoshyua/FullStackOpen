1fba7e64ad817bec00a57ed8bfe2a6e285c81ced
Joshua Fielding <josh.e.fielding@gmail.com>
2023-03-15 06:36:33 +1100

    part3 - mongo connection semiworking

--- part3/course/backend/mongo.js
+++ part3/course/backend/mongo.js
@@ -6,28 +6,37 @@ if (process.argv.length < 3) {
 }
 
 const password = process.argv[2];
-
-// const url =
-//   `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
-
 const url = `mongodb+srv://josh:${password}@cluster0.can6okv.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose.set('strictQuery', false);
-mongoose.connect(url);
+// rework this into a try catch
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB Connected');
+    note.save();
+    console.log('New note added!');
+  })
+  .catch((error) => {
+    console.log(`Oh no, error: ${error}`);
+    // mongoose.connection.close();
+  });
+// .then(() => {
+//   mongoose.disconnect();
+// });
 
 const noteSchema = new mongoose.Schema({
   content: String,
+  date: Date,
   important: Boolean,
 });
 
 const Note = mongoose.model('Note', noteSchema);
 
 const note = new Note({
-  content: 'HTML is Easy',
+  content: 'Adding even more test content is fun fun fun!',
+  date: new Date(),
   important: true,
-});
-
-note.save().then((result) => {
-  console.log('note saved!');
-  mongoose.connection.close();
 });
