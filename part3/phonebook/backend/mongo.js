3b998ef3acbcc9fa21bf0e7f64798118224a1f51
Joshua Fielding <josh.e.fielding@gmail.com>
2023-03-17 09:46:00 +1100

    part3 - more failures

--- part3/phonebook/backend/mongo.js
+++ part3/phonebook/backend/mongo.js
@@ -13,71 +13,78 @@ if (!(process.argv.length === 3 || process.argv.length === 5)) {
 }
 
 const password = process.argv[2];
-const url = `mongodb+srv://josh:${password}@cluster0.lalfif0.mongodb.net/phonebook?retryWrites=true&w=majority`;
-
 const name = process.argv[3];
 const number = process.argv[4];
+const url = `mongodb+srv://josh:${password}@cluster0.lalfif0.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
 
 // establishes the schema to be used in the database
 const personSchema = new mongoose.Schema({
   name: String,
-  number: String,
+  number: Number,
 });
 
 const Person = mongoose.model('Person', personSchema);
 
 // creates a new person object based on the Person model constructor function
-// const person = new Person({
-//   name: 'Sam Smith',
-//   number: '000-111-222-333',
-//   date: new Date(),
-// });
-
 const person = new Person({
   name: name,
   number: number,
-  date: new Date(),
 });
 
-const dbConnect = async () => {
-  let db = null;
-  try {
-    await mongoose.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+if (process.argv.length === 3) {
+  console.log('phonebook:');
+  Person.find({}).then((persons) => {
+    persons.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
     });
-    db = mongoose.connection;
-    console.log('datbase connected!');
-    let dbRes = await person.save();
-    // close the connection after note is sent
-    db.close();
-    return dbRes;
-  } catch (error) {
-    db && db.close();
-    console.log(`Error in database connection: ${error}`);
-    throw error;
-  }
-};
-
-dbConnect()
-  .then((res) => {
-    // console.log(res);
-    // if (!name || !number) {
-    //   return;
-    // }
-    // console.log(db);
-    console.log(`addded ${name} number ${number} to phonebook`);
-    // console.log(`Connected to db: ${res}`)
-  })
-  .catch((error) => {
-    console.log(`Error calling: ${error}`);
+    mongoose.connection.close();
   });
+} else if (process.argv.length === 5) {
+  person.save().then((result) => {
+    console.log(`result: ${result}`);
+    console.log(`added ${personName} number ${personNumber} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  console.log(
+    `Please either enter 3 entries e.g. node mongo.js password or 5 entries e.g. node mongo.js password TestName 000-111`
+  );
+  process.exit();
+}
 
-// if (process.argv.length === 3) {
-//   console.log('only 3 args provided, map through Person model and show all entries');
-//   // search through collection here?
-//   Person.find({}).then((persons) => {
-//     console.log(persons);
+// const dbConnect = async (person) => {
+//   let db = null;
+//   try {
+//     await mongoose.connect(url, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     db = mongoose.connection;
+//     console.log('datbase connected!');
+//     if (person) {}
+//     let dbRes = await person.save();
+//     // close the connection after note is sent
+//     db.close();
+//     return dbRes;
+//   } catch (error) {
+//     db && db.close();
+//     console.log(`Error in database connection: ${error}`);
+//     throw error;
+//   }
+// };
+
+// dbConnect()
+//   .then((res) => {
+//     // console.log(res);
+//     // if (!name || !number) {
+//     //   return;
+//     // }
+//     // console.log(db);
+//     console.log(`addded ${name} number ${number} to phonebook`);
+//     // console.log(`Connected to db: ${res}`)
+//   })
+//   .catch((error) => {
+//     console.log(`Error calling: ${error}`);
 //   });
-//   // close connection ?
-// }
