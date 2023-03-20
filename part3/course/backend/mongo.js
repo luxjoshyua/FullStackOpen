27058db80d034894b2833f53e7cd76d9fe241dc0
Joshua Fielding <josh.e.fielding@gmail.com>
2023-03-20 18:00:20 +1100

    part3 - notes - rewrote using async

--- part3/course/backend/mongo.js
+++ part3/course/backend/mongo.js
@@ -1,17 +1,14 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const content = process.argv[2];
 
-if (process.argv.length < 3) {
-  console.log('give password as argument');
+if (process.argv.length < 2) {
+  // console.log('give password as argument');
+  console.log('give node and filename as arguments');
   process.exit(1);
 }
 
-const password = process.argv[2];
-
-const url = `mongodb+srv://josh:${password}h@cluster0.can6okv.mongodb.net/noteApp?retryWrites=true&w=majority`;
-
-mongoose.set('strictQuery', false);
-mongoose.connect(url);
-
+// establishes the schema to be used in the database
 const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
@@ -20,22 +17,50 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema);
 
-const note = new Note({
-  content: 'Mongoose makes things easy',
-  date: new Date(),
-  important: true,
-});
+const connect = async () => {
+  console.log('Connecting...');
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log('Connected!');
+};
 
-/*
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
-*/
-
-Note.find({}).then((result) => {
-  result.forEach((note) => {
-    console.log(note);
-  });
+const closeConnection = async () => {
   mongoose.connection.close();
-});
+};
+
+const saveNote = async (content) => {
+  const note = new Note({ content: content, date: new Date(), important: true });
+  await note.save();
+  console.log(`Added content ${content} to note`);
+};
+
+const loadNote = async () => {
+  const allNotes = await Note.find({}).then((result) => {
+    if (!result) {
+      return;
+    } else if (result.length === 1) {
+      console.log(`One note in collection: ${result}`);
+    } else if (result.length > 1) {
+      result.forEach((note) => {
+        console.log(`Note in note collection ${note.content}`);
+      });
+    }
+  });
+  // const allNotes = await Note.find({});
+  return allNotes;
+};
+
+const main = async () => {
+  await connect();
+
+  if (process.argv.length === 2) {
+    await loadNote();
+  }
+
+  if (process.argv.length === 3) {
+    await saveNote(content);
+  }
+
+  await closeConnection();
+};
+
+main();
