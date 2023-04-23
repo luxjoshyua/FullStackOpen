949b8a969aeeac3a3efea8f4ae3e955e35b00042
Joshua Fielding <josh.e.fielding@gmail.com>
2023-04-24 06:30:45 +1000

    part4 - notes - more tests semiworking

--- part4/course/backend/tests/note_api.test.js
+++ part4/course/backend/tests/note_api.test.js
@@ -1,60 +1,44 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
-const helper = require('./test_helper');
 const app = require('../app');
 
-// wrap the app with the supertest function into a superagent object
-// https://github.com/ladjs/superagent
 const api = supertest(app);
 
-console.log('API', api);
-
 const Note = require('../models/note');
+const helper = require('./test_helper.test');
 
-// to run a specific test:
-// $ npm test -- -t 'notes are returned as json'
-
-// clean up the database on each test
 beforeEach(async () => {
   await Note.deleteMany({});
-  // create a new note object
+
   let noteObject = new Note(helper.initialNotes[0]);
-  // save the note object to the database
   await noteObject.save();
 
   noteObject = new Note(helper.initialNotes[1]);
-  await noteObject.save();
-});
+  return await noteObject.save();
+}, 20000);
 
 test('notes are returned as json', async () => {
   await api
     .get('/api/notes')
-    // check the response status code
     .expect(200)
-    // check the header is set to appplication/json i.e. make sure the data is in the desired format
     .expect('Content-Type', /application\/json/);
-}, 10000); // change the default timeout of 5 seconds to 10 seconds
+}, 20000);
 
-// test that the database has 2 notes
 test('all notes are returned', async () => {
-  // store the response of the request to the response variable
   const response = await api.get('/api/notes');
-  // execution gets here only after the HTTP request is complete
-  // the result of HTTP request is saved in the variable response
+
   expect(response.body).toHaveLength(helper.initialNotes.length);
 });
 
 test('a specific note is within the returned notes', async () => {
   const response = await api.get('/api/notes');
 
-  // used to create an array containing the content of every note returned by the API
   const contents = response.body.map((r) => r.content);
-  // toContain method is used for checking that the note given to it as a parameter is in the list of notes returned by the API
-  // https://jestjs.io/docs/expect#tocontainitem
-  expect(contents).toContain('Browser can only execute JavaScript');
+
+  expect(contents).toContain('Browser can execute only JavaScript');
 });
 
-test('a valid note can be added', async () => {
+test('a valid note can be added ', async () => {
   const newNote = {
     content: 'async/await simplifies making async calls',
     important: true,
@@ -69,9 +53,7 @@ test('a valid note can be added', async () => {
   const notesAtEnd = await helper.notesInDb();
   expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1);
 
-  // return an array containing the content of every note returned by the API
   const contents = notesAtEnd.map((n) => n.content);
-
   expect(contents).toContain('async/await simplifies making async calls');
 });
 
@@ -82,13 +64,41 @@ test('note without content is not added', async () => {
 
   await api.post('/api/notes').send(newNote).expect(400);
 
-  // check the state stored in the database after the saving operation, by fetching all of the notes of the application
   const notesAtEnd = await helper.notesInDb();
 
   expect(notesAtEnd).toHaveLength(helper.initialNotes.length);
 });
 
-// close the database connection after all tests have been run
+test('a specific note can be viewed', async () => {
+  const notesAtStart = await helper.notesInDb();
+
+  const noteToView = notesAtStart[0];
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+
+  //const processedNoteToView = JSON.parse(JSON.stringify(noteToView))
+
+  expect(resultNote.body).toEqual(noteToView);
+});
+
+test('a note can be deleted', async () => {
+  const notesAtStart = await helper.notesInDb();
+  const noteToDelete = notesAtStart[0];
+
+  await api.delete(`/api/notes/${noteToDelete.id}`).expect(204);
+
+  const notesAtEnd = await helper.notesInDb();
+
+  expect(notesAtEnd).toHaveLength(helper.initialNotes.length - 1);
+
+  const contents = notesAtEnd.map((r) => r.content);
+
+  expect(contents).not.toContain(noteToDelete.content);
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
