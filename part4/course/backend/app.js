78f417b45d26ef4acd09dbf90f036048907414f6
luxjoshyua <josh.e.fielding@gmail.com>
2023-04-26 08:41:22 +1000

    part4 - course - rework

--- part4/course/backend/app.js
+++ part4/course/backend/app.js
@@ -1,34 +1,52 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
-require('express-async-errors')
+const config = require('./utils/config');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
+require('express-async-errors');
 
-const notesRouter = require('./controllers/notes')
-const middleware = require('./utils/middleware')
+const notesRouter = require('./controllers/notes');
+const middleware = require('./utils/middleware');
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 
-logger.info('connecting to', config.MONGODB_URI)
+logger.info('URL in use', config.MONGODB_URI);
 
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
+// mongoose
+//   .connect(config.MONGODB_URI)
+//   .then(() => {
+//     logger.info('connected to MongoDB');
+//   })
+//   .catch((error) => {
+//     logger.error('error connecting to MongoDB:', error.message);
+//   });
 
-app.use(cors())
-app.use(express.static('build'))
-app.use(express.json())
-app.use(middleware.requestLogger)
+const connect = async () => {
+  console.log('connecting...');
+  await mongoose.connect(config.MONGODB_URI);
+  console.log('connected');
+  // try {
 
-app.use('/api/notes', notesRouter)
+  // } catch (error) {
+  //   logger.error('error connecting to MongoDB:', error.message);
+  // }
+};
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+const main = async () => {
+  await connect();
+};
 
-module.exports = app
+main();
+
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use('/api/notes', notesRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
