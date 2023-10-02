eba7119cc9f7aeee5d0887a6a0480eabe580ea6c
Josh <josh.e.fielding@gmail.com>
2023-10-03 06:43:03 +1100

    added part8 - mongoose model

--- part8/course-react-graphql/backend/index.js
+++ part8/course-react-graphql/backend/index.js
@@ -2,6 +2,24 @@ const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const { v1: uuid } = require('uuid')
 const { GraphQLError } = require('graphql')
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+const Person = require('./models/person')
+
+require('dotenv').config()
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+console.log('connecting to...', MONGODB_URI)
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 let persons = [
   {
@@ -73,47 +91,69 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    personCount: () => persons.length,
-    allPersons: (root, args) => {
+    personCount: async () => Person.collection.countDocuments,
+    allPersons: async (root, args) => {
       if (!args.phone) {
-        return persons
+        return Person.find({})
       }
-      const byPhone = (person) => (args.phone === 'YES' ? person.phone : !person.phone)
-      return persons.filter(byPhone)
+
+      // if parameter has YES value, will return all resutls containing phone
+      // if parameter has NO value, will return all results not containing phone
+      return Person.find({ phone: { $exists: args.phone === 'YES' } })
+
+      // apollo waits for the promise to resolve, then sends back the value which the Promise resolves to,
+      // so it is like this:
+      // const result = await Person.find({})
+      // return result
+
+      // if (!args.phone) {
+      //   return persons
+      // }
+      // const byPhone = (person) => (args.phone === 'YES' ? person.phone : !person.phone)
+      // return persons.filter(byPhone)
     },
-    findPerson: (root, args) => persons.find((p) => p.name === args.name),
+    // findPerson: async (root, args) => persons.find((p) => p.name === args.name),
+    findPerson: async (root, args) => Person.findOne({ name: args.name }),
   },
   Person: {
-    address: ({ street, city }) => {
+    address: (root) => {
       return {
-        street,
-        city,
+        street: root.street,
+        city: root.city,
       }
     },
   },
   Mutation: {
-    addPerson: (root, args) => {
-      if (persons.find((p) => p.name === args.name)) {
-        throw new GraphQLError('Name must be unique', {
+    addPerson: async (root, args) => {
+      // spread the new object in
+      const person = new Person({ ...args })
+
+      try {
+        await person.save()
+      } catch (error) {
+        throw new GraphQLError('Saving person failed', {
           extensions: {
             code: 'BAD_USER_INPUT',
             invalidArgs: args.name,
+            error,
           },
         })
       }
-      const person = { ...args, id: uuid() }
-      persons = persons.concat(person)
+
       return person
     },
-    editNumber: (root, args) => {
-      const person = persons.find((p) => p.name === args.name)
-      if (!person) {
-        return null
-      }
+    editNumber: async (root, args) => {
+      const person = await Person.findOne({ name: args.name })
+      person.phone = args.phone
+      return person.save()
+      // const person = persons.find((p) => p.name === args.name)
+      // if (!person) {
+      //   return null
+      // }
 
-      const updatedPerson = { ...person, phone: args.phone }
-      persons = persons.map((p) => (p.name === args.name ? updatedPerson : p))
-      return updatedPerson
+      // const updatedPerson = { ...person, phone: args.phone }
+      // persons = persons.map((p) => (p.name === args.name ? updatedPerson : p))
+      // return updatedPerson
     },
   },
 }
