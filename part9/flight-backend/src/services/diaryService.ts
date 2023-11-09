a55df3f2fd2b6717345625667227c4d7f122eee5
Josh <josh.e.fielding@gmail.com>
2023-11-10 06:43:18 +1100

    part9 - preventing accidental undefined result

--- part9/flight-backend/src/services/diaryService.ts
+++ part9/flight-backend/src/services/diaryService.ts
@@ -19,8 +19,17 @@ const addDiary = () => {
   return null;
 };
 
+// return single diaryentry
+// no guarantee specified id can be found, so throws error
+const findById = (id: number): DiaryEntry | undefined => {
+  // find method returns undefined if the object is not found
+  const entry = diaries.find((d) => d.id === id);
+  return entry;
+};
+
 export default {
   getEntries,
   addDiary,
   getNonSensitiveEntries,
+  findById,
 };
