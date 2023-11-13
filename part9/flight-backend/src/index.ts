b74e77874ae4a4d90a1af1f24a07fc45c9eb2bbe
Josh <josh.e.fielding@gmail.com>
2023-11-14 05:37:42 +1100

    part9 - post request working on flight-backend

--- part9/flight-backend/src/index.ts
+++ part9/flight-backend/src/index.ts
@@ -1,14 +1,20 @@
 import express from "express";
+const app = express();
 import diaryRouter from "./routes/diaries";
 
-const app = express();
 app.use(express.json());
 
 const PORT = 3000;
 
-// app.get("/ping", (_req, res) => {
-//   console.log("someone pinged here");
-//   res.send("pong");
+app.get("/ping", (_req, res) => {
+  console.log("someone pinged here");
+  res.send("pong");
+});
+
+// app.post("/api/diaries", (req, res) => {
+//   console.log(req.body);
+//   console.log("posting something");
+//   return res.status(200).send("POST").end();
 // });
 
 app.use("/api/diaries", diaryRouter);
