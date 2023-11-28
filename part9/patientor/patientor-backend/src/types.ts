c733d543c18947424e12a7b0a653acc5038122a2
Josh <josh.e.fielding@gmail.com>
2023-11-29 08:26:36 +1100

    part9 - completed 9.22

--- part9/patientor/patientor-backend/src/types.ts
+++ part9/patientor/patientor-backend/src/types.ts
@@ -10,8 +10,6 @@ export enum Gender {
   Other = "other",
 }
 
-export interface Entry {}
-
 export interface Patient {
   id: string;
   name: string;
@@ -21,6 +19,50 @@ export interface Patient {
   occupation: string;
   entries: Entry[];
 }
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  // optional
+  // refer to the Diagnosis type, specifically the code field of the Diagnosis type
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export type NonSSNPatientEntry = Omit<Patient, "ssn">;
 
